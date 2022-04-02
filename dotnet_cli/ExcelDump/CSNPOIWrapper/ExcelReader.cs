09b4b0d3434e8fe920f37c61a28dcd734390939d
morita-nteg <64726147+morita-nteg@users.noreply.github.com>
2022-04-02 23:57:13 +0900

    NPOI Example

---
+++ dotnet_cli/ExcelDump/CSNPOIWrapper/ExcelReader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPOI.SS.UserModel;
+
+namespace CSNPOIWrapper
+{
+    public class ExcelReader
+    {
+        public static void DumpConsole(string excelFileName, int n)
+        {
+            try
+            {
+                IWorkbook workbook = WorkbookFactory.Create(excelFileName);
+                ISheet worksheet = workbook.GetSheetAt(n);
+                int lastRow = worksheet.LastRowNum;
+                for (int i = 0; i <= lastRow; i++)
+                {
+                    IRow row = worksheet.GetRow(i);
+                    int lastCell = row.LastCellNum;
+                    string[] cellValues = new string[lastCell];
+                    for (int j = 0; j < lastCell; j++)
+                    {
+                        ICell cell = row?.GetCell(j);
+                        if (cell != null)
+                        {
+                            switch (cell?.CellType)
+                            {
+                                case CellType.String:
+                                    cellValues[j] = cell?.StringCellValue;
+                                    break;
+                                case CellType.Numeric:
+                                    cellValues[j] = cell?.NumericCellValue.ToString();
+                                    break;
+                                default:
+                                    break;
+                            }
+
+                        }
+                    }
+                    Console.WriteLine(String.Join(",", cellValues));
+                }
+                Console.WriteLine();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ dotnet_cli/ExcelDump/ExcelDump/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelDump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSNPOIWrapper.ExcelReader.DumpConsole(args[0], 0);
+        }
+    }
+}
