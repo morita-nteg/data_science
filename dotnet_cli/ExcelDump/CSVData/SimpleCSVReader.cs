75c4c85a7b2f9e321328f66b0558be5c69b8b98a
morita-nteg <64726147+morita-nteg@users.noreply.github.com>
2022-04-03 22:21:53 +0900

    Append CSVReader

---
+++ dotnet_cli/ExcelDump/CSVData/SimpleCSVReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+
+namespace CSVData
+{
+    public class SimpleCSVReader
+    {
+        private string csvFilePath = "";
+        private List<string[]> csvLines = null;
+        public SimpleCSVReader(string csvFilePath)
+        {
+            this.csvFilePath = csvFilePath;
+            csvLines = File.ReadAllLines(csvFilePath).Where(line => !string.IsNullOrWhiteSpace(line)).Skip(1).Select(line => line.Split(',')).ToList();
+        }
+
+        private double[] NormalizedArrays(string[] token, int offset)
+        {
+            double[] ret = new double[token.Length + offset];
+            double total = double.Parse(token[1]);
+
+            for (int i = 2; i < token.Length; i++)
+            {
+                double elem = double.Parse(token[i]);
+                ret[i + offset] = elem * elem / total;
+            }
+            return ret;
+        }
+
+        public List<double[]> ToValues()
+        {
+            List<double[]> csvValues = new List<double[]>();
+            csvLines.ForEach(line => csvValues.Add(NormalizedArrays(line, 0)));
+            return csvValues;
+        }
+
+        public void DumpNormalizedArrays()
+        {
+            List<double[]> normalizedArrays = ToValues();
+            normalizedArrays.ForEach(narr => Console.WriteLine(String.Join(",", narr)));
+        }
+    }
+}
---
+++ dotnet_cli/ExcelDump/CSVDump/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVDump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleCSVReader r = new SimpleCSVReader(args[0]);
+            r.DumpNormalizedArrays();
+        }
+    }
+}
